4eed2b083701ffdbd41b43a195f199c8e3c32f82
pxlxinzhao <pxlxinzhao@gmail.com>
2015-09-03 17:05:12 -0400

    not enought time, have to commit

---
+++ js/social-config.js
@@ -0,0 +1,35 @@
+/**
+ * Created by patrickpu on 9/3/2015.
+ */
+
+ServiceConfiguration.configurations.remove({
+    service: 'facebook'
+});
+
+ServiceConfiguration.configurations.insert({
+    service: 'facebook',
+    appId: '899773626737808',
+    secret: 'e6b0bd5198c5544fa4527575b3bec9be'
+});
+
+if (Meteor.isClient) {
+
+    Template.login.events({
+        'click #facebook-login': function(event) {
+            Meteor.loginWithFacebook({}, function(err){
+                if (err) {
+                    throw new Meteor.Error("Facebook login failed");
+                }
+            });
+        },
+
+        'click #logout': function(event) {
+            Meteor.logout(function(err){
+                if (err) {
+                    throw new Meteor.Error("Logout failed");
+                }
+            })
+        }
+    });
+
+}
