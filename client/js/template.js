a725097ffb46d618a7bf861bb53c66023501f0b7
pxlxinzhao <pxlxinzhao@gmail.com>
2015-09-22 17:41:15 -0400

    work on message

--- client/js/template.js
+++ client/js/template.js
@@ -413,6 +413,15 @@ if (Meteor.isClient){
 
 
 //-- MESSAGES
+    Template.messages.onRendered(function () {
+        //$('.button-collapse').sideNav({
+        //        menuWidth: 300, // Default is 240
+        //        edge: 'right', // Choose the horizontal origin
+        //        closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+        //    }
+        //);
+    });
+
     Template.messages.helpers({
         getMessage: function(){
             //console.log('Session messaggSender: ',Session.get('MessageSender'));
